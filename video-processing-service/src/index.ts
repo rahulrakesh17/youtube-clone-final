7f6332eb8fd612acd206ef7e097790fbeb2d4126
rahulrakesh17 <rahulrakesh1702@gmail.com>
2024-08-24 09:24:41 +0530

    Internal/Local video processing ffmpeg

---
+++ video-processing-service/src/index.ts
@@ -0,0 +1,33 @@
+import express from 'express';
+import ffmpeg from 'fluent-ffmpeg';
+
+
+const app = express();
+const port = 3000;
+
+app.post('/process-video', (req, res) => {
+  //we need a body to pass a post request
+  //get the path of the input video file from the req body
+  const inputFilePath = req.body.inputFilePath;
+  const outputFilePath = req.body.outputFilePath;
+
+  if(!inputFilePath||!outputFilePath){
+    res.status(400).send("Bad request: Missing file path");
+  }
+
+  ffmpeg(inputFilePath)
+    .outputOptions("-vf","scale=-1:360") // converting to 360p(found from ffmpeg document)
+    .on("end",()=>{
+
+    })
+    .on("error",(err)=>{
+      console.log(`An error occured: ${err.message}`);
+      res.status(500).send(`Internal server error: ${err.message}`);
+    })
+    .save(outputFilePath);
+});
+
+app.listen(port, () => {
+  console.log(
+    `video processing service running at http://localhost:${port}`);
+});
