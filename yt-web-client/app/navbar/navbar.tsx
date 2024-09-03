cf059505040c52fc75f51e0ff43b4d6a1843427c
rahulrakesh17 <rahulrakesh1702@gmail.com>
2024-09-03 19:14:10 +0530

    Logo Update

--- yt-web-client/app/navbar/navbar.tsx
+++ yt-web-client/app/navbar/navbar.tsx
@@ -8,8 +8,8 @@ export default function Navbar() {
   return (
     <nav className={styles.nav}>
       <Link href="/" className={styles.logoContainer}>
-            <Image width={100} height={60}
-                src="/youtube-logo-png-46020.png" alt="YouTube Logo"/>
+            <Image width={150} height={60}
+                src="/YouTube-Logo.wine.svg" alt="YouTube Logo"/>
       </Link>
     </nav>
   );
